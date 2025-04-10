c9aabd24f80570715ede48af95836294a33175c3
Amos Omariba <amomariba@gmail.com>
2025-04-10 22:38:25 +0300

    Initial commit

--- bookstore.sql
+++ bookstore.sql
@@ -1,2 +1,11 @@
 CREATE DATABASE bookstore;
 USE bookstore;
+
+CREATE TABLE books (id PRIMARY KEY auto increment,
+author VARCHAR(100),
+title VARCHAR(100),
+price DECIMAL(5,2),
+quantity INT);
+
+INSERT INTO books (author, title, price, quantity) VALUES
+('J.K. Rowling', 'Harry Potter and the Philosopher''s Stone', 19.99, 100);
